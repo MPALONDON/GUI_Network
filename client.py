29ce9cd429e4bd1e9b231fbd4dcd7c5a4ececdbf
MPALONDON <info@mpalondon.co.uk>
2025-11-01 17:36:58 +0000

    completed the if flags on process messages function.updated the ui and added datetime

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import tkinter, socket, threading, json
 from tkinter import StringVar
+from datetime import datetime
 
 root = tkinter.Tk()
 root.title("Chat Client")
@@ -7,7 +8,7 @@ root.geometry("800x800")
 root.resizable(False,False)
 
 my_font = ("SimSun",14)
-black = "#010101"
+black = "#000000"
 light_green = "#1fc742"
 white = "#ffffff"
 red = "#ff3855"
@@ -16,7 +17,16 @@ yellow = "#fff700"
 green = "#1fc742"
 blue = "#5dadec"
 purple = "#9c51b6"
-root.config(bg=black)
+cream = "#FDFBD4"
+caramel = "#D89549"
+
+root.iconbitmap("./favicon/Web_.ico")
+root.config(bg=cream)
+
+def date_format():
+    today_date = datetime.today()
+    format_date = today_date.strftime("%d-%m-%Y %I:%M%p")
+    return format_date
 
 class Connection:
     def __init__(self):
@@ -41,74 +51,138 @@ def connect(connection):
         my_listbox.insert(0,"Connection not established...")
 
 def disconnect(connection):
-    pass
+    message_packet = create_message(
+        flag = "DISCONNECT", name = connection.name, message = "I am Leaving", colour = connection.colour, date = date_format())
+    message_json = json.dumps(message_packet)
+    connection.client_socket.send(message_json.encode(connection.encoder))
 
 def gui_start():
-    pass
+    connect_button.config(state="disabled")
+    disconnect_button.config(state="normal")
+    send_button.config(state="normal")
+    name_entry.config(state="disabled")
+    ip_entry.config(state="disabled")
+    port_entry.config(state="disabled")
+
+    for button in colour_buttons:
+        button.config(state="disabled")
 
 def gui_end():
-    pass
+    connect_button.config(state="normal")
+    disconnect_button.config(state="disabled")
+    send_button.config(state="disabled")
+    name_entry.config(state="normal")
+    ip_entry.config(state="normal")
+    port_entry.config(state="normal")
 
-def create_message(flag, name, message, colour):
-    pass
+    for button in colour_buttons:
+        button.config(state="normal")
+
+def create_message(flag, name, message, colour, date):
+    message_packet = {
+        "flag": flag,
+        "name": name,
+        "message": message,
+        "colour": colour,
+        "date": date
+    }
+    return message_packet
 
 def process_message(connection, message_json):
-    pass
+    message_packet = json.loads(message_json)
+    flag = message_packet["flag"]
+    name = message_packet["name"]
+    message = message_packet["message"]
+    colour = message_packet["colour"]
+    date = message_packet["date"]
+
+    if flag == "INFO":
+        message_packet = create_message(
+            flag = "INFO",name = connection.name,message = f"Joins the server!", colour = connection.colour, date = date_format())
+        message_json = json.dumps(message_packet)
+        connection.client_socket.send(message_json.encode(connection.encoder))
+        gui_start()
+
+        receive_thread = threading.Thread(target=receive_message,args=(connection,))
+        receive_thread.start()
+
+    elif flag =="MESSAGE":
+        my_listbox.insert(0, f"{date} {name}: {message}")
+        my_listbox.itemconfig(0, fg = colour)
+
+    elif flag =="DISCONNECT":
+        my_listbox.insert(0,f"{date} {name}: {message}")
+        my_listbox.itemconfig(0, fg = colour)
+        disconnect(connection)
+
+    else:
+        my_listbox.insert(0,"Error processing message")
 
 def send_message(connection):
-    pass
+    message_packet = create_message(flag = "MESSAGE",name = connection.name,
+                                                message = input_entry.get(),colour=connection.colour, date = date_format())
+    message_json = json.dumps(message_packet)
+    connection.client_socket.send(message_json.encode(connection.encoder))
+
+    input_entry.delete(0,"end")
 
 def receive_message(connection):
-    pass
+    while True:
+        try:
+            message_json = connection.client_socket.recv(connection.byte_size)
+            process_message(connection, message_json)
+        except:
+            my_listbox.insert(0,"Connection has been closed...")
+            break
 
 
 
-info_frame = tkinter.Frame(root, bg=black)
-colour_frame = tkinter.Frame(root, bg=black)
-output_frame = tkinter.Frame(root, bg=black)
-input_frame = tkinter.Frame(root,bg=black)
+info_frame = tkinter.Frame(root, bg=cream)
+colour_frame = tkinter.Frame(root, bg=cream)
+output_frame = tkinter.Frame(root, bg=cream)
+input_frame = tkinter.Frame(root,bg=cream)
 
 info_frame.pack()
 colour_frame.pack()
 output_frame.pack(pady=10)
 input_frame.pack()
 
-name_label = tkinter.Label(info_frame, text="Client Name: ", font=my_font, fg=light_green,bg=black)
+name_label = tkinter.Label(info_frame, text="Client Name: ", font=my_font, fg=light_green,bg=cream)
 name_entry = tkinter.Entry(info_frame,borderwidth=3, font=my_font)
-ip_label = tkinter.Label(info_frame,text="Host IP:", font=my_font,fg=light_green,bg=black)
+ip_label = tkinter.Label(info_frame,text="Host IP:", font=my_font,fg=light_green,bg=cream)
 ip_entry = tkinter.Entry(info_frame,borderwidth=3,font=my_font)
-port_label = tkinter.Label(info_frame, text="Port Num:", font=my_font, fg=light_green,bg=black)
+port_label = tkinter.Label(info_frame, text="Port Num:", font=my_font, fg=light_green,bg=cream)
 port_entry = tkinter.Entry(info_frame, borderwidth=3, font=my_font, width=10)
 
 connect_button = tkinter.Button(info_frame, text="Connect", font=my_font,bg=light_green, borderwidth=5, width=10,
                                 command=lambda:connect(my_connection))
 disconnect_button = tkinter.Button(info_frame, text="Disconnect", font=my_font,bg=light_green, borderwidth=5, width=10,
-                                   state="disabled",command=disconnect)
+                                   state="disabled",command=lambda:disconnect(my_connection))
 
 
 colour = StringVar()
 colour.set(white)
 
 white_button = tkinter.Radiobutton(colour_frame,width=5, text="white",variable=colour, value=white,
-                                   bg=black, fg=light_green,font=my_font)
+                                   bg=cream, fg=light_green,font=my_font)
 
 red_button = tkinter.Radiobutton(colour_frame,width=5, text="red",variable=colour, value=red,
-                                   bg=black, fg=light_green,font=my_font)
+                                   bg=cream, fg=light_green,font=my_font)
 
 orange_button = tkinter.Radiobutton(colour_frame,width=5, text="orange",variable=colour, value=orange,
-                                   bg=black, fg=light_green,font=my_font)
+                                   bg=cream, fg=light_green,font=my_font)
 
 yellow_button = tkinter.Radiobutton(colour_frame,width=5, text="yellow",variable=colour, value=yellow,
-                                   bg=black, fg=light_green,font=my_font)
+                                   bg=cream, fg=light_green,font=my_font)
 
 green_button = tkinter.Radiobutton(colour_frame,width=5, text="green",variable=colour, value=green,
-                                   bg=black, fg=light_green,font=my_font)
+                                   bg=cream, fg=light_green,font=my_font)
 
 blue_button = tkinter.Radiobutton(colour_frame,width=5, text="blue",variable=colour, value=blue,
-                                   bg=black, fg=light_green,font=my_font)
+                                   bg=cream, fg=light_green,font=my_font)
 
 purple_button = tkinter.Radiobutton(colour_frame,width=5, text="purple",variable=colour, value=purple,
-                                   bg=black, fg=light_green,font=my_font)
+                                   bg=cream, fg=light_green,font=my_font)
 
 colour_buttons = [white_button,red_button,orange_button,yellow_button,green_button,blue_button,purple_button]
 
@@ -137,7 +211,7 @@ my_listbox.grid(row=0,column=0)
 my_scrollbar.grid(row=0, column=1,sticky="ns")
 
 input_entry = tkinter.Entry(input_frame, width=45, borderwidth=3, font=my_font)
-send_button = tkinter.Button(input_frame,text="send",borderwidth=5, width=10, font=my_font, bg=light_green, state="disabled")
+send_button = tkinter.Button(input_frame,text="send",borderwidth=5, width=10, font=my_font, bg=light_green, state="disabled", command=lambda:send_message(my_connection))
 
 input_entry.grid(row=0, column=0, padx=5, pady=5)
 send_button.grid(row=0, column=1, padx=5, pady=5)
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import json
 import tkinter,socket,threading
+from datetime import datetime
 
 root = tkinter.Tk()
 root.title("Chat Server")
@@ -7,9 +8,16 @@ root.geometry("800x800")
 root.resizable(False,False)
 
 my_font = ("SimSun",14)
-black = "#010101"
-red = "#FF0000"
-root.config(bg=black)
+cream = "#FDFBD4"
+caramel = "#D89549"
+black = "#000000"
+root.iconbitmap("./favicon/Web_.ico")
+root.config(bg=cream)
+
+def date_format():
+    today_date = datetime.today()
+    format_date = today_date.strftime("%d-%m-%Y %I:%M%p")
+    return format_date
 
 class Connection:
     def __init__(self):
@@ -28,7 +36,7 @@ def start_server(connection):
     connection.server_socket.listen()
 
     history_listbox.delete(0,"end")
-    history_listbox.insert(0,f"Server started on port {connection.port}")
+    history_listbox.insert(0,f"{date_format()} Server started on port {connection.port}")
     end_button.config(state="normal")
     self_broadcast_button.config(state="normal")
     message_button.config(state="normal")
@@ -40,21 +48,35 @@ def start_server(connection):
 
 
 def end_server(connection):
-    pass
+    message_packet = create_message(flag = "DISCONNECT", name = "ADMIN (broadcast)",
+                                    message = "Server is closing...", colour = caramel, date = date_format())
+    message_json = json.dumps(message_packet)
+    broadcast_message(connection,message_json.encode(connection.encoder))
+
+    history_listbox.insert(0,f"Server started on port {connection.port}")
+    end_button.config(state="disabled")
+    self_broadcast_button.config(state="disabled")
+    message_button.config(state="disabled")
+    kick_button.config(state="disabled")
+    ban_button.config(state="disabled")
+    start_button.config(state="normal")
+
+    connection.server_socket.close()
+
 
 def connect_client(connection):
     while True:
         try:
             client_socket, client_address = connection.server_socket.accept()
             if client_address[0] in connection.banned_ips:
-                message_packet = create_message(flag = "DISCONNECT",name = "Admin (private)",
-                                                message = "You have been banned...",colour=red)
+                message_packet = create_message(flag = "DISCONNECT", name = "ADMIN (private)",
+                                                message = "You have been banned...", colour=caramel, date = date_format())
                 message_json = json.dumps(message_packet)
                 client_socket.send(message_json.encode(connection.encoder))
                 client_socket.close()
             else:
-                message_packet = create_message(flag = "INFO",name = "Admin (private)",
-                                                message = "Please send your name...",colour=red)
+                message_packet = create_message(flag = "INFO", name = "ADMIN (private)",
+                                                message = "Please send your name...", colour=caramel, date = date_format())
                 message_json = json.dumps(message_packet)
                 client_socket.send(message_json.encode(connection.encoder))
                 message_json = client_socket.recv(connection.byte_size)
@@ -64,12 +86,13 @@ def connect_client(connection):
             break
 
 
-def create_message(flag,name,message,colour):
+def create_message(flag,name,message,colour, date):
     message_packet = {
         "flag": flag,
         "name": name,
         "message": message,
-        "colour": colour
+        "colour": colour,
+        "date" : date
     }
     return message_packet
 
@@ -79,23 +102,35 @@ def process_message(connection,message_json,client_socket,client_address=(0,0)):
     name = message_packet["name"]
     message = message_packet["message"]
     colour = message_packet["colour"]
+    date = message_packet["date"]
 
     if flag == "INFO":
         connection.client_sockets.append(client_socket)
         connection.client_ips.append(client_address[0])
         message_packet = create_message(flag = "MESSAGE", name = "ADMIN (broadcast)",
-                                        message = f"{name} has joined the server!!!", colour = red)
+                                        message = f"{name} has joined the server!!!", colour = caramel, date = date_format())
         message_json = json.dumps(message_packet)
         broadcast_message(connection,message_json.encode(connection.encoder))
-        client_listbox.insert("end",f"Name: {name}         IP ADDR: {client_address[0]}")
-        receive_thread = threading.Thread(target = receive_message, args=(connection,client_socket,))
-        receive_thread.start()
+        client_listbox.insert("end",f"{date} Name: {name}  IP ADDR: {client_address[0]}")
+        threading.Thread(target = receive_message, args=(connection,client_socket,)).start()
 
     elif flag == "MESSAGE":
-        pass
+        broadcast_message(connection, message_json)
+
+        history_listbox.insert(0, f"{date} {name}: {message}")
+        history_listbox.itemconfig(0, fg=colour)
 
     elif flag == "DISCONNECT":
-        pass
+        index = connection.client_sockets.index(client_socket)
+        connection.client_sockets.remove(client_socket)
+        connection.client_ips.pop(index)
+        client_listbox.delete(index,"end")
+        client_socket.close()
+        message_packet = create_message(
+            flag = "MESSAGE",name = "ADMIN (broadcast)",message = f"{name}: has left the server...", colour = caramel, date = date_format())
+        message_json = json.dumps(message_packet)
+        broadcast_message(connection = connection,message_json = message_json.encode(connection.encoder))
+        history_listbox.insert(0, f"{date} ADMIN (broadcast): {name} has left the server...")
 
     else:
         history_listbox.insert(0, "Error processing message...")
@@ -105,26 +140,53 @@ def broadcast_message(connection,message_json):
         client_socket.send(message_json)
 
 def receive_message(connection, client_socket):
-    pass
+    while True:
+        try:
+            message_json = client_socket.recv(connection.byte_size)
+            process_message(
+                connection = connection,message_json = message_json,client_socket = client_socket)
+        except:
+            break
 
 def self_broadcast(connection):
-    pass
+    message_packet = create_message(
+            flag = "MESSAGE",name = "ADMIN (broadcast)",message = input_entry.get(), colour = caramel, date = date_format())
+    message_json = json.dumps(message_packet)
+    broadcast_message(connection = connection,message_json = message_json.encode(connection.encoder))
+    input_entry.delete(0,"end")
 
 def private_message(connection):
-    pass
+    index = client_listbox.curselection()[0]
+    client_socket = connection.client_sockets[index]
+    message_packet = create_message(
+            flag = "MESSAGE",name = "ADMIN (private)",message = input_entry.get(), colour = caramel, date = date_format())
+    message_json = json.dumps(message_packet)
+    client_socket.send(message_json.encode(connection.encoder))
+    input_entry.delete(0,"end")
 
 def kick_client(connection):
-    pass
+    index = client_listbox.curselection()[0]
+    client_socket = connection.client_sockets[index]
+    message_packet = create_message(
+            flag = "DISCONNECT",name = "ADMIN (private)",message = "You have been kicked...", colour = caramel, date = date_format())
+    message_json = json.dumps(message_packet)
+    client_socket.send(message_json.encode(connection.encoder))
 
 def ban_client(connection):
-    pass
+    index = client_listbox.curselection()[0]
+    client_socket = connection.client_sockets[index]
+    message_packet = create_message(
+            flag = "DISCONNECT",name = "ADMIN (private)",message = "You have been banned...", colour = caramel, date = date_format())
+    message_json = json.dumps(message_packet)
+    client_socket.send(message_json.encode(connection.encoder))
+    connection.banned_ips.append(connection.client_ips[index])
 
 
-connection_frame = tkinter.Frame(root,bg=black)
-history_frame = tkinter.Frame(root,bg=black)
-client_frame = tkinter.Frame(root,bg=black)
-message_frame = tkinter.Frame(root,bg=black)
-admin_frame = tkinter.Frame(root,bg=black)
+connection_frame = tkinter.Frame(root, bg=cream)
+history_frame = tkinter.Frame(root, bg=cream)
+client_frame = tkinter.Frame(root, bg=cream)
+message_frame = tkinter.Frame(root, bg=cream)
+admin_frame = tkinter.Frame(root, bg=cream)
 
 connection_frame.pack(pady=5)
 history_frame.pack()
@@ -133,13 +195,13 @@ message_frame.pack()
 admin_frame.pack()
 
 post_label = tkinter.Label(
-    connection_frame, text="Port Number:",font=my_font, bg=black, fg=red)
+    connection_frame, text="Port Number:",font=my_font, bg=cream, fg=black)
 port_entry = tkinter.Entry(
     connection_frame,width=10, borderwidth=3, font=my_font)
 start_button = tkinter.Button(
-    connection_frame,text="Start Server", borderwidth=5, width=15,font=my_font, bg=red, command=lambda:start_server(my_connection))
+    connection_frame,text="Start Server", borderwidth=5, width=15,font=my_font, bg=caramel, command=lambda:start_server(my_connection))
 end_button = tkinter.Button(
-    connection_frame, text="End Server", borderwidth=5, width=15,font=my_font, bg=red, state="disabled")
+    connection_frame, text="End Server", borderwidth=5, width=15,font=my_font, bg=caramel, state="disabled", command=lambda:end_server(my_connection))
 
 post_label.grid(row=0, column=0,padx=2, pady=10)
 port_entry.grid(row=0, column=1,padx=2, pady=10)
@@ -148,7 +210,7 @@ end_button.grid(row=0, column=3,padx=5, pady=10)
 
 history_scrollbar = tkinter.Scrollbar(history_frame,orient="vertical")
 history_listbox = tkinter.Listbox(history_frame, height=10, width=55, borderwidth=3,
-                                  font=my_font,bg=black,fg=red, yscrollcommand=history_scrollbar.set)
+                                  font=my_font, bg=black, fg=caramel, yscrollcommand=history_scrollbar.set, )
 history_scrollbar.config(command=history_listbox.yview)
 
 history_listbox.grid(row=0, column=0)
@@ -157,7 +219,7 @@ history_scrollbar.grid(row=0, column=0, sticky="ns")
 
 client_scrollbar = tkinter.Scrollbar(client_frame,orient="vertical")
 client_listbox = tkinter.Listbox(client_frame, height=10, width=55, borderwidth=3,
-                                  font=my_font,bg=black,fg=red, yscrollcommand=client_scrollbar.set)
+                                 font=my_font, bg=black, fg=caramel, yscrollcommand=client_scrollbar.set)
 client_scrollbar.config(command=client_listbox.yview)
 
 
@@ -168,17 +230,17 @@ client_scrollbar.grid(row=0, column=0, sticky="ns")
 input_entry = tkinter.Entry(message_frame, width=40, borderwidth=3, font=my_font)
 
 self_broadcast_button = tkinter.Button(
-    message_frame, text="Broadcast", width=13, borderwidth=5, font=my_font,bg=red,state="disabled")
+    message_frame, text="Broadcast", width=13, borderwidth=5, font=my_font,bg=caramel,state="disabled", command=lambda:self_broadcast(my_connection))
 
 input_entry.grid(row=0,column=0, padx=5, pady=5)
 self_broadcast_button.grid(row=0,column=1, padx=5, pady=5)
 
 message_button = tkinter.Button(
-    admin_frame, text="PM", borderwidth=5, width=15, font=my_font, bg=red, state="disabled")
+    admin_frame, text="PM", borderwidth=5, width=15, font=my_font, bg=caramel, state="disabled", command=lambda:private_message(my_connection))
 kick_button = tkinter.Button(
-    admin_frame, text="Kick", borderwidth=5, width=15, font=my_font, bg=red, state="disabled")
+    admin_frame, text="Kick", borderwidth=5, width=15, font=my_font, bg=caramel, state="disabled", command = lambda: kick_client(my_connection))
 ban_button = tkinter.Button(
-    admin_frame, text="Ban", borderwidth=5, width=15, font=my_font, bg=red, state="disabled")
+    admin_frame, text="Ban", borderwidth=5, width=15, font=my_font, bg=caramel, state="disabled", command = lambda: ban_client(my_connection))
 
 message_button.grid(row=0, column=0, padx=5, pady=5)
 kick_button.grid(row=0, column=1, padx=5, pady=5)
